d7178365f651dd53f29e987d71d16114a58390fd
Afonso Trindade <alctrindade2005@gmail.com>
2026-05-16 17:35:21 +0100

    feat: response on void get_patient response

--- procedures/patients/get_patient.sql
+++ procedures/patients/get_patient.sql
@@ -4,7 +4,13 @@ BEGIN
     IF p_id IS NULL THEN
         SELECT * FROM Patient;
     ELSE
+    
+		IF NOT EXISTS (SELECT 1 FROM Patient WHERE id = p_id) THEN
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Patient not found.';
+		END IF;
+        
         SELECT * FROM Patient WHERE id = p_id;
+        
     END IF;
 END$$
 DELIMITER ;
