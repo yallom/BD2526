a8b26139c3fa072fd2c3fa0fddcc84ba819e0d34
Afonso Trindade <alctrindade2005@gmail.com>
2026-05-18 22:42:12 +0100

    feat: location procedures

---
+++ procedures/locations/get_location.sql
@@ -0,0 +1,21 @@
+DELIMITER $$
+
+CREATE PROCEDURE get_location(
+    IN p_id      INT,
+    IN p_number  INT,
+    IN p_floor   INT,
+    IN p_state   ENUM('available', 'unavailable', 'maintenance', 'inoperable')
+)
+BEGIN
+
+    SELECT *
+    FROM Location
+    WHERE
+        (p_id      IS NULL OR id     = p_id)
+    AND (p_number  IS NULL OR number = p_number)
+    AND (p_floor   IS NULL OR floor  = p_floor)
+    AND (p_state   IS NULL OR state  = p_state);
+
+END$$
+
+DELIMITER ;
